cc1598a915c4dbdcba545fb1210d288e71e33551
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-06-06 04:00:48 +0200

    Implemented DocFX ignore attribute in DIV2.Format.Exporter assembly
    - Attribute designed to ease discard documented elements when DocFX generate the site documentation.

---
+++ DIV2.Format.Exporter/Utils/DocFxIgnoreAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DIV2.Format.Exporter.Utils
+{
+    /// <summary>
+    /// Ignore this element on DocFX build process.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = false)]
+    [DocFxIgnore]
+    internal class DocFxIgnoreAttribute : Attribute
+    {
+    }
+}
